71d0ca3716b4749eb3c8e3e37da8e315a1204b89
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-09-06 16:04:48 +0800

    's'

--- BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_bs128x8.sh
+++ BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_bs128x8.sh
@@ -2,7 +2,7 @@
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/train.py \
 --model BigGAN_inv \
 --hidden 128 \
---dataset I128_hdf5 --parallel --shuffle  --num_workers 8 --batch_size 8 \
+--dataset I128_hdf5 --parallel --shuffle  --num_workers 0 --batch_size 256 \
 --num_G_accumulations 8 --num_D_accumulations 8 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
 --G_attn '64' --D_attn '64' \
