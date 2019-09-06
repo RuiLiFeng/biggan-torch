3e27fc54005eba3b6d1a470fe29d82e189c99f5e
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-09-07 03:10:41 +0800

    's'

--- BigGAN-PyTorch-master/BigGAN.py
+++ BigGAN-PyTorch-master/BigGAN.py
@@ -437,6 +437,8 @@ class G_D(nn.Module):
     # If real data is provided, concatenate it with the Generator's output
     # along the batch dimension for improved efficiency.
     else:
+      print(G_z)
+      print(x)
       D_input = torch.cat([G_z, x], 0) if x is not None else G_z
       D_class = torch.cat([gy, dy], 0) if dy is not None else gy
       # Get Discriminator output
