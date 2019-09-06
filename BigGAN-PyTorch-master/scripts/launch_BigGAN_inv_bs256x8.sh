32e5367fcdeefa16c94a7b5196bf49072db08bc2
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-09-07 01:57:37 +0800

    's'

--- BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_bs256x8.sh
+++ BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_bs256x8.sh
@@ -5,14 +5,14 @@ python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/tr
 --num_G_accumulations 8 --num_D_accumulations 8 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
 --G_attn 64 --D_attn 64 \
---G_ch 128 --D_ch 128 \
---G_depth 2 --D_depth 2 \
 --G_nl inplace_relu --D_nl inplace_relu \
 --SN_eps 1e-6 --BN_eps 1e-5 --adam_eps 1e-6 \
 --G_ortho 0.0 \
 --G_shared \
 --G_init ortho --D_init ortho \
---hier --dim_z 128 --shared_dim 128 \
---ema --use_ema --ema_start 20000 --G_eval_mode \
---test_every 2000 --save_every 500 --num_best_copies 5 --num_save_copies 2 --seed 0 \
+--hier --dim_z 120 --shared_dim 128 \
+--G_eval_mode \
+--G_ch 96 --D_ch 96 \
+--ema --use_ema --ema_start 20000 \
+--test_every 2000 --save_every 1000 --num_best_copies 5 --num_save_copies 2 --seed 0 \
 --use_multiepoch_sampler \
