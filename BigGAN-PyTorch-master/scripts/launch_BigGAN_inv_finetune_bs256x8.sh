2972d846bc528c85dc08acd3e46e13ce9b9f783b
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-09-08 23:13:10 +0800

    's'

--- BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_finetune_bs256x8.sh
+++ BigGAN-PyTorch-master/scripts/launch_BigGAN_inv_finetune_bs256x8.sh
@@ -2,7 +2,7 @@
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/train.py \
 --model BigGAN_inv \
 --experiment_name BigGAN_inv_finetue100k \
---resume True --load_weights 100k \
+--resume --load_weights 100k \
 --dataset I128_hdf5 --parallel --shuffle  --num_workers 0 --batch_size 256 \
 --num_G_accumulations 8 --num_D_accumulations 8 \
 --num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
