637b347e3fd7cb90c2c3a3447357b78a6099d416
ruilifeng <frl1996@mail.ustc.edu.cn>
2019-09-09 12:47:06 +0800

    's'

--- BigGAN-PyTorch-master/scripts/utils/prepare_data.sh
+++ BigGAN-PyTorch-master/scripts/utils/prepare_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/make_hdf5.py \
---dataset I128 --batch_size 8 --data_root "/gpub/imagenet_raw"
+--dataset I128 --batch_size 8 --data_root "/gpub/temp/imagenet2012/downloads/extracted"
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/calculate_inception_moments.py \
 --dataset I128_hdf5
