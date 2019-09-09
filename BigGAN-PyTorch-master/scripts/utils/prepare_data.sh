dbc57c400e43293f9b91a84a370d025b755a21e6
frl <frl1996@mail.ustc.edu.cn>
2019-09-09 16:19:30 +0800

    's'

--- BigGAN-PyTorch-master/scripts/utils/prepare_data.sh
+++ BigGAN-PyTorch-master/scripts/utils/prepare_data.sh
@@ -2,4 +2,4 @@
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/make_hdf5.py \
 --dataset I128 --batch_size 8 --data_root "/gpub/temp/imagenet2012/downloads/extracted"
 python /ghome/fengrl/home/biggan/torch-imp/biggan-torch/BigGAN-PyTorch-master/calculate_inception_moments.py \
---dataset I128_hdf5
+--dataset I128_hdf5 --batch_size 8
